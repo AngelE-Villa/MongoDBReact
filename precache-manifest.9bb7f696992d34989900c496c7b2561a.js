25d680a0aa622fe104894df9a498d149784d1a04
Angel Villa <angel.villa.est@tecazuay.edu.ec>
2022-03-21 22:00:46 -0500

    Updates

---
+++ precache-manifest.9bb7f696992d34989900c496c7b2561a.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "436e86816cbd856b0fa6785bed1e1e8c",
+    "url": "/MongoDBReact/index.html"
+  },
+  {
+    "revision": "068665e4aeaee97c0925",
+    "url": "/MongoDBReact/static/css/2.e94d0158.chunk.css"
+  },
+  {
+    "revision": "ca87cad7ad64d9b88a5a",
+    "url": "/MongoDBReact/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "068665e4aeaee97c0925",
+    "url": "/MongoDBReact/static/js/2.ab8e4705.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/MongoDBReact/static/js/2.ab8e4705.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca87cad7ad64d9b88a5a",
+    "url": "/MongoDBReact/static/js/main.9fdaa362.chunk.js"
+  },
+  {
+    "revision": "b52b40c7ca250e7a06a6",
+    "url": "/MongoDBReact/static/js/runtime-main.cdaa6a4a.js"
+  },
+  {
+    "revision": "c7a33805ffda0d32bd2a9904c8b02750",
+    "url": "/MongoDBReact/static/media/color.c7a33805.png"
+  },
+  {
+    "revision": "567f57385ea3dde2c9aec797d07850d2",
+    "url": "/MongoDBReact/static/media/line.567f5738.gif"
+  },
+  {
+    "revision": "177cc92d2e8027712a8c1724abd272cd",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-300.177cc92d.ttf"
+  },
+  {
+    "revision": "27ef0b062b2e221df16f3bbd97c2dca8",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-300.27ef0b06.svg"
+  },
+  {
+    "revision": "521d17bc9f3526c690e8ada6eee55bec",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-300.521d17bc.woff"
+  },
+  {
+    "revision": "60c866748ff15f5b347fdba64596b1b1",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-300.60c86674.woff2"
+  },
+  {
+    "revision": "76b56857ebbae3a5a689f213feb11af0",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-300.76b56857.eot"
+  },
+  {
+    "revision": "148a6749baa5f658a45183ddb5ee159f",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-700.148a6749.eot"
+  },
+  {
+    "revision": "2e00b2635b51ba336b4b67a5d0bc03c7",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-700.2e00b263.svg"
+  },
+  {
+    "revision": "623e3205570002af47fc2b88f9335d19",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-700.623e3205.woff"
+  },
+  {
+    "revision": "7e08cc656863d52bcb5cd34805ac605b",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-700.7e08cc65.ttf"
+  },
+  {
+    "revision": "d08c09f2f169f4a6edbcf8b8d1636cb4",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-700.d08c09f2.woff2"
+  },
+  {
+    "revision": "7aab4c13671282c90669eb6a10357e41",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-regular.7aab4c13.svg"
+  },
+  {
+    "revision": "9dce7f01715340861bdb57318e2f3fdc",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-regular.9dce7f01.eot"
+  },
+  {
+    "revision": "bf2d0783515b7d75c35bde69e01b3135",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-regular.bf2d0783.woff"
+  },
+  {
+    "revision": "c045b73d86803686f4cd1cc3f9ceba59",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-regular.c045b73d.ttf"
+  },
+  {
+    "revision": "cffb686d7d2f4682df8342bd4d276e09",
+    "url": "/MongoDBReact/static/media/open-sans-v15-latin-regular.cffb686d.woff2"
+  },
+  {
+    "revision": "121254f73060bcbb53ca13258dbd134f",
+    "url": "/MongoDBReact/static/media/primeicons.121254f7.ttf"
+  },
+  {
+    "revision": "25954ab5e32c01d577dcafd035597f5d",
+    "url": "/MongoDBReact/static/media/primeicons.25954ab5.svg"
+  },
+  {
+    "revision": "3b6e3706f42d8876fe364ab4d75683fd",
+    "url": "/MongoDBReact/static/media/primeicons.3b6e3706.woff"
+  },
+  {
+    "revision": "b0f5d02f4e70967dcc8162e938484366",
+    "url": "/MongoDBReact/static/media/primeicons.b0f5d02f.eot"
+  }
+]);
